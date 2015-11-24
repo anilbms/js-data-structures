758fa3a1f27eacb3e47a6c9934130d495cbf7fa8
anilbms <anilbms75@gmail.com>
2015-11-24 13:11:56 +0530

    shallowCopy and DeepCopy demo and test case update.

--- js/arrayCopy.js
+++ js/arrayCopy.js
@@ -1,3 +1,37 @@
-function shallowCopy (array1, array2) {
+function arrayShallowCopy (array1, array2) {
+	// shallow copy
+	array2 = array1;
+	return array2;
+}
+
+function arrayShallowCopyModifyFirstArgumentArray (array1, array2) {
+	// shallow copy
+	array2 = array1;
+
+	array1.push(7);
+	console.log("shallow copy array 1",array1);
+	console.log("shallow copy array 2", array2);
+	return array2;
+}
+
+
+function arrayDeepCopy (array1, array2) {
+	for (var i = array1.length - 1; i >= 0; i--) {
+		array2[i] = array1[i];
+	}
+
+	return array2;
+}
+
+
+function arrayDeepCopyModifyFirstArgumentArray (array1, array2) {
 	
+	for (var i = array1.length - 1; i >= 0; i--) {
+		array2[i] = array1[i];
+	}
+
+	array1.push(7);
+	console.log("deep copy array 1",array1);
+	console.log("deep copy array 2", array2);
+	return array2;
 }
--- specs/arrayCopy.spec.js
+++ specs/arrayCopy.spec.js
@@ -1,5 +1,27 @@
-describe('Method: methodName', function() {
-    it('sortMenu method should do ...', function() {
-        expect(true).toBe(true);
+describe('Test for Array shallow copy', function() {
+    it('arrayShallowCopy should be defined', function() {
+        expect((typeof arrayShallowCopy) === 'function').toBe(true);
+    });
+
+    it('arrayShallowCopy should perform shallow copy', function() {
+    	var array1 = [1,2,3],
+    		array2 = [];
+        expect(arrayShallowCopy(array1, array2)).toEqual(array1);
+    });
+    it('arrayShallowCopyModifyFirstArgumentArray should perform shallow copy', function() {
+    	var array1 = [1,2,3],
+    		array2 = [];
+        expect(arrayShallowCopyModifyFirstArgumentArray(array1, array2)).toEqual(array1);
+    });
+
+    it('arrayDeepCopy should perform shallow copy', function() {
+    	var array1 = [1,2,3],
+    		array2 = [];
+        expect(arrayDeepCopy(array1, array2)).toEqual(array1);
+    });
+    it('arrayDeepCopyModifyFirstArgumentArray should perform shallow copy', function() {
+    	var array1 = [1,2,3],
+    		array2 = [];
+        expect(arrayDeepCopyModifyFirstArgumentArray(array1, array2)).not.toEqual(array1);
     });
 });
